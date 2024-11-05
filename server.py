ac86c8695481dfa3f92705b21cfd1235e1efc10c
MusaTamzid05 <tamzid101@gmail.com>
2024-11-06 04:01:10 +0600

    Add Processor in the catch all routes

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask import jsonify
+from lib.processor import Processor
 
+
+processor = Processor(data_format_path="./data_format.json", words_path="./random.txt")
 
 app = Flask(__name__)
 
 @app.route("/", defaults={"path" : ""})
 @app.route("/<path:path>")
 def catch_all(path):
-    return f"{path}"
+    print(path)
+    response = processor.process(route=path)
+    return jsonify(response)
 
